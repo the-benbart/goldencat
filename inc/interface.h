11778b5b4fb015c690f7d7e86159cf525776cc67
the-benbart <benbart@benbart.net>
2015-08-21 14:50:54 +0200

    mainMenu Completed optMenu work in progress

--- inc/interface.h
+++ inc/interface.h
@@ -4,7 +4,7 @@ int menuScroll;
 int menuScrollLevel()
   {
     if ( menuScroll == 1 ) { mainMenu(); }
-    else if ( menuScroll == 2 ) { optMenu(); }
+    //else if ( menuScroll == 2 ) { optMenu(); }
     else { /*screen(ErrmenuScrollLevel)*/; delay(1000); mainMenu(); }
   }
 int menuTicket(int cursor)
@@ -32,7 +32,7 @@ int menuBehavor()
 }
 int mainMenu()
   {
-    int menuScroll = 1;
+    menuScroll = 1;
     menuBehavor();
     if ( choix == NULL ) { choix = 1; }
     else if ( choix >= 3 ) { choix = 3; }
@@ -64,17 +64,90 @@ int mainMenu()
   menuTicket(3);
   LcdString (" Calibrer");
   delay(100);
+  LcdClear();
 
 }
 void optMenu()
 {
-  LcdString("optMenu works");
+  menuScroll = 2;
+  menuBehavor();
+  if ( choix == NULL ) { choix = 1; }
+  else if ( choix >= 4 ) { choix = 4; }
+  else if ( choix <= 1 ) { choix = 1; }
+  if ( menuScroll == 3 )
+   {
+     switch (choix)
+     {
+       case 1:
+       optSpeedUp();
+       break;
+       case 2:
+       optSpeedDown();
+       break;
+       case 3:
+       optIncubTime();
+       break;
+       case 4:
+       optAlertType();
+       break;
+     }
+   }
+LcdClear();
+gotoXY(5,0);
+LcdString (" Menu/options");
+gotoXY(0,2);
+menuTicket(1);
+LcdString (" Vitesse Monté");
+gotoXY(0,3);
+menuTicket(2);
+LcdString (" Vitesse Descente");
+gotoXY(0,4);
+menuTicket(3);
+LcdString (" Temps incubation");
+gotoXY(0,5);
+menuTicket(4);
+LcdString (" Type d'alerte");
+delay(100);
 }
 void steve()
 {
+  LcdClear();
   LcdString("steve");
+  delay(1000);
+  LcdClear();
 }
 void doCalibrate()
 {
+  LcdClear();
   LcdString("doCalibrate");
+  delay(1000);
+  LcdClear();
+}
+void optSpeedUp()
+{
+  LcdClear();
+  LcdString("optSpeedUp");
+  delay(1000);
+  LcdClear();
+}
+void optSpeedDown()
+{
+  LcdClear();
+  LcdString("optSpeedDown");
+  delay(1000);
+  LcdClear();
+}
+void optIncubTime()
+{
+  LcdClear();
+  LcdString("optIncubTime");
+  delay(1000);
+  LcdClear();
+}
+void optAlertType()
+{
+  LcdClear();
+  LcdString("optAlertType");
+  delay(1000);
+  LcdClear();
 }
